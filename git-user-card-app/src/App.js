3d798c64d49ac9daabfcd310d8cdc48d8249b88e
JOliver23 <59593045+JOliver23@users.noreply.github.com>
2020-05-08 17:15:34 -0700

    App.js structured, ready for api call and methods

--- git-user-card-app/src/App.js
+++ git-user-card-app/src/App.js
@@ -1,9 +1,38 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div />
-  );
+class App extends React.Component {
+  state = {
+    user: [],
+    error: "",
+  }
+  // CDM() {
+  //   fetch("https://api.github.com/users/JOliver23")
+  //     .then(response => response.json())
+  //     .then(user => {
+  //       console.log("jo: App.js: App: CDM: response github api", user.message);
+  //     })
+  //     .catch(err => {
+  //       console.error("jo: App.js: App: CDM: fetch failed")
+  //     })
+  // }
+
+  // handleChange = e => {
+  //   this.setState({
+  //     open/close maybe???
+  //   })
+  // }
+
+  render() {
+    return(
+      <div className="App">
+        <UserCard 
+          //user={this.state.user}
+          //any other props i may pass
+        />
+      </div>
+    )
+  }
+
 }
 
 export default App;
