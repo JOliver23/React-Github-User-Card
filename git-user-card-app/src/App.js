ef682efff7c53aad864b376e09b86e23718c5d1b
JOliver23 <59593045+JOliver23@users.noreply.github.com>
2020-05-08 17:31:14 -0700

    App.js title

--- git-user-card-app/src/App.js
+++ git-user-card-app/src/App.js
@@ -5,7 +5,8 @@ class App extends React.Component {
     user: [],
     error: "",
   }
-  // CDM() {
+  componentDidMount() {
+      console.log("jo: App.js: App: CDM: CDM run")
   //   fetch("https://api.github.com/users/JOliver23")
   //     .then(response => response.json())
   //     .then(user => {
@@ -14,7 +15,7 @@ class App extends React.Component {
   //     .catch(err => {
   //       console.error("jo: App.js: App: CDM: fetch failed")
   //     })
-  // }
+  }
 
   // handleChange = e => {
   //   this.setState({
@@ -23,8 +24,10 @@ class App extends React.Component {
   // }
 
   render() {
+    console.log("jo: App.js: App: render run");
     return(
       <div className="App">
+        <h1>GitHub User Card APP!!!</h1>
         <UserCard 
           //user={this.state.user}
           //any other props i may pass
