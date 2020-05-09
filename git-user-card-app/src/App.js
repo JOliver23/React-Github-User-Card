b5cbed88b941fe4935762f74c0afe0184b9185af
JOliver23 <59593045+JOliver23@users.noreply.github.com>
2020-05-08 18:35:20 -0700

    added input to App.js, including handlers for state changes

--- git-user-card-app/src/App.js
+++ git-user-card-app/src/App.js
@@ -5,6 +5,7 @@ class App extends React.Component {
   state = {
     user: [],
     error: "",
+    userName: "JOliver23"
   }
   componentDidMount() {
       console.log("jo: App.js: App: CDM: CDM run")
@@ -12,23 +13,59 @@ class App extends React.Component {
       .then(response => response.json())
       .then(user => {
         console.log("jo: App.js: App: CDM: response github api", user);
+        if (user.type !== "User") {
+          this.setState({error: "Error in CDM fetch of given userName"})
+        } else {
+          this.setState({user: user})
+        }
       })
       .catch(err => {
         console.error("jo: App.js: App: CDM: fetch failed")
-      })
-  }
+      });
+  };
 
-  // handleChange = e => {
-  //   this.setState({
-  //     open/close maybe???
-  //   })
-  // }
+  componentDidUpdate(prevProps, PrevState) {
+    console.log("jo: App.js: App: CDU: Cdu render")
+  };
+
+  handleChange = e => {
+    this.setState({
+      userName: e.target.value
+    });
+  };
+
+  handleUserNameChange = e => {
+    fetch(`https://api.github.com/users/${this.state.userName}`)
+      .then(response => {
+        console.log(response);
+        return response.json();
+      })
+      .then(user => {
+        if (user.login === "error") {
+          this.setState({error: "broken"});
+        } else {
+          this.setState({user: user})
+        }
+      })
+      .catch(err => {
+        console.log("jo: App.js: App: handleUserNameChange: fetch failed: ", err);
+        this.setState({error: err});
+      });
+  };
+
+
 
   render() {
     console.log("jo: App.js: App: render run");
     return(
       <div className="App">
         <h1>GitHub User Card APP!!!</h1>
+        <input 
+          type="text"
+          placeholder="Enter your GitHub login name"
+          value={this.state.userName}
+          onChange={this.handleChange}
+        />
         <UserCard 
           //user={this.state.user}
           //any other props i may pass
--- git-user-card-app/src/components/UserCard.js
+++ git-user-card-app/src/components/UserCard.js
@@ -6,17 +6,17 @@ function UserCard(props) {
     console.log("jo: UserCard.js: UserCard: render run");
     return (
         <div>
-            <img src={"this.state.user.avatar_url"} alt="github user image"/>
+            <img src={"this.state.user.avatar_url"} alt="github user avatar"/>
             <h2>{"this.state.user.name"}</h2>
             <h4>Username: {"this.state.user.login"}</h4>
             <div className="gen-info closed">
-                <p>Repositories: {"this.state.user public_repos"}</p>
+                <p>Location: {"this.state.user.location"}</p>
+                <p>Repositories: {"this.state.user.public_repos"}</p>
                 <p>Followers: {"this.state.user.followers"}</p>
                 <p>Following: {"this.state.user.following"}</p>
                 <p>GitHub: {"this.state.user.html_url"}</p>
             </div>
-            <button>+++ expand +++</button> //onClick => toggle className'closed' display none
-
+            <button>+++ expand +++</button> <span>onClick => toggle className'closed' display none</span>
         </div>
     );
 };
